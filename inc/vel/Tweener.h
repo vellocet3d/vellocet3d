5c769367ad15379b85f58f5c79dee51fc142d8c0
jason <jason@youtorque.com>
2022-10-14 17:43:55 -0500

    refactored Tweener class from it's previously over complicated implementation that I put together before I understood vectors had directions and lengths

--- inc/vel/Tweener.h
+++ inc/vel/Tweener.h
@@ -5,35 +5,22 @@
 
 namespace vel
 {
-	// TODO: my god this is possibly the worst most overly complicated implementation of tweening between two
-	// vectors one could possibly implement. I did this very early on before I really understood vector directions
-	// and lengths apparently...this MUST be re-implemented
 	class Tweener
 	{
 	private:
 		glm::vec3	fromVec;
 		glm::vec3	toVec;
+		float		distance;
 		glm::vec3	currentVec;
 		float		speed;
-		glm::vec3	speeds;
-
-		int			forwardXDirection; // -1, 1, 0
-		int			backwardXDirection; // -1, 1, 0
-		int			forwardYDirection; // -1, 1, 0
-		int			backwardYDirection; // -1, 1, 0
-		int			forwardZDirection; // -1, 1, 0
-		int			backwardZDirection; // -1, 1, 0
-		
-		void		setDirections();
-		void		setSpeeds();
+		float		lerpVal;
 
 		bool		forwardUpdateComplete;
 		bool		backwardUpdateComplete;
-		void		updateCompletionStatus();
 
 	public:
 		Tweener(glm::vec3 from, glm::vec3 to, float speed);
-		
+
 		glm::vec3	updateForward(float dt);
 		glm::vec3	updateBackward(float dt);
 		glm::vec3	getCurrentVec();
--- src/Tweener.cpp
+++ src/Tweener.cpp
@@ -1,9 +1,8 @@
-
+#define GLM_FORCE_ALIGNED_GENTYPES
+#include <glm/gtx/string_cast.hpp>
+#include "glm/gtx/compatibility.hpp"
 
 #include "vel/Tweener.h"
-
-
-
 
 namespace vel
 {
@@ -13,22 +12,13 @@ namespace vel
 	Tweener::Tweener(glm::vec3 from, glm::vec3 to, float speed) :
 		fromVec(from),
 		toVec(to),
+		distance(glm::distance(from, to)),
 		currentVec(from),
 		speed(speed),
-		forwardXDirection(0),
-		backwardXDirection(0),
-		forwardYDirection(0),
-		backwardYDirection(0),
-		forwardZDirection(0),
-		backwardZDirection(0),
+		lerpVal(0.0f),
 		forwardUpdateComplete(false),
 		backwardUpdateComplete(true)
-	{
-		
-		this->setDirections();
-		this->setSpeeds();
-
-	};
+	{};
 
 	void Tweener::reset()
 	{
@@ -47,185 +37,44 @@ namespace vel
 		return this->backwardUpdateComplete;
 	}
 
-	void Tweener::updateCompletionStatus()
-	{
-		//if (this->currentVec == this->toVec)
-		//{
-		//	this->forwardUpdateComplete = true;
-		//}
-		//else
-		//{
-		//	this->forwardUpdateComplete = false;
-		//}
-
-		//if (this->currentVec == this->fromVec)
-		//{
-		//	this->backwardUpdateComplete = true;
-		//}
-		//else
-		//{
-		//	this->backwardUpdateComplete = false;
-		//}
-
-		if (this->currentVec == this->toVec)
-		{
-			this->forwardUpdateComplete = true;
-			this->backwardUpdateComplete = false;
-			return;
-		}
-
-		if (this->currentVec == this->fromVec)
-		{
-			this->backwardUpdateComplete = true;
-			this->forwardUpdateComplete = false;
-		}
-
-	}
-
 	glm::vec3 Tweener::updateForward(float dt)
 	{
-		this->updateCompletionStatus();
-
 		if (!this->forwardUpdateComplete)
 		{
-			// update this->currentVec
-			this->currentVec.x += this->forwardXDirection * (this->speeds.x * dt);
-			this->currentVec.y += this->forwardYDirection * (this->speeds.y * dt);
-			this->currentVec.z += this->forwardZDirection * (this->speeds.z * dt);
+			this->lerpVal += (1.0f / (this->distance / this->speed)) * dt;
 
-			// correct x positions
-			if (this->forwardXDirection == 1)
+			if (this->lerpVal >= 1.0f)
 			{
-				if (this->currentVec.x > this->toVec.x)
-				{
-					this->currentVec.x = this->toVec.x;
-				}
+				this->currentVec = this->toVec;
+				this->lerpVal = 0.0f;
+				this->forwardUpdateComplete = true;
+				this->backwardUpdateComplete = false;
 			}
-			else if (this->forwardXDirection == -1)
+			else
 			{
-				if (this->currentVec.x < this->toVec.x)
-				{
-					this->currentVec.x = this->toVec.x;
-				}
-			}
-			else // 0
-			{
-				this->currentVec.x = this->fromVec.x;
-			}
-
-			// correct y positions
-			if (this->forwardYDirection == 1)
-			{
-				if (this->currentVec.y > this->toVec.y)
-				{
-					this->currentVec.y = this->toVec.y;
-				}
-			}
-			else if (this->forwardYDirection == -1)
-			{
-				if (this->currentVec.y < this->toVec.y)
-				{
-					this->currentVec.y = this->toVec.y;
-				}
-			}
-			else // 0
-			{
-				this->currentVec.y = this->fromVec.y;
-			}
-
-			// correct z positions
-			if (this->forwardZDirection == 1)
-			{
-				if (this->currentVec.z > this->toVec.z)
-				{
-					this->currentVec.z = this->toVec.z;
-				}
-			}
-			else if (this->forwardZDirection == -1)
-			{
-				if (this->currentVec.z < this->toVec.z)
-				{
-					this->currentVec.z = this->toVec.z;
-				}
-			}
-			else // 0
-			{
-				this->currentVec.z = this->fromVec.z;
+				this->currentVec = glm::lerp(this->fromVec, this->toVec, this->lerpVal);
 			}
 		}
-		
+
 		return this->currentVec;
 	}
 
 	glm::vec3 Tweener::updateBackward(float dt)
 	{
-		this->updateCompletionStatus();
-
 		if (!this->backwardUpdateComplete)
 		{
-			// update this->currentVec
-			this->currentVec.x += this->backwardXDirection * (this->speeds.x * dt);
-			this->currentVec.y += this->backwardYDirection * (this->speeds.y * dt);
-			this->currentVec.z += this->backwardZDirection * (this->speeds.z * dt);
+			this->lerpVal += (1.0f / (this->distance / this->speed)) * dt;
 
-			// correct x positions
-			if (this->backwardXDirection == 1)
+			if (this->lerpVal >= 1.0f)
 			{
-				if (this->currentVec.x > this->fromVec.x)
-				{
-					this->currentVec.x = this->fromVec.x;
-				}
+				this->currentVec = this->fromVec;
+				this->lerpVal = 0.0f;
+				this->forwardUpdateComplete = false;
+				this->backwardUpdateComplete = true;
 			}
-			else if (this->backwardXDirection == -1)
+			else
 			{
-				if (this->currentVec.x < this->fromVec.x)
-				{
-					this->currentVec.x = this->fromVec.x;
-				}
-			}
-			else // 0
-			{
-				this->currentVec.x = this->fromVec.x;
-			}
-
-			// correct y positions
-			if (this->backwardYDirection == 1)
-			{
-				if (this->currentVec.y > this->fromVec.y)
-				{
-					this->currentVec.y = this->fromVec.y;
-				}
-			}
-			else if (this->backwardYDirection == -1)
-			{
-				if (this->currentVec.y < this->fromVec.y)
-				{
-					this->currentVec.y = this->fromVec.y;
-				}
-			}
-			else // 0
-			{
-				this->currentVec.y = this->fromVec.y;
-			}
-
-			// correct z positions
-			if (this->backwardZDirection == 1)
-			{
-				if (this->currentVec.z > this->fromVec.z)
-				{
-					this->currentVec.z = this->fromVec.z;
-				}
-			}
-			else if (this->backwardZDirection == -1)
-			{
-				if (this->currentVec.z < this->fromVec.z)
-				{
-					this->currentVec.z = this->fromVec.z;
-				}
-			}
-			else // 0
-			{
-				this->currentVec.z = this->fromVec.z;
+				this->currentVec = glm::lerp(this->toVec, this->fromVec, this->lerpVal);
 			}
 		}
 
@@ -237,97 +86,9 @@ namespace vel
 		return this->currentVec;
 	}
 
-	void Tweener::setDirections()
-	{
-		// set forward directions
-		if ((this->toVec.x - this->fromVec.x) > 0.0f)
-		{
-			this->forwardXDirection = 1;
-		}
-		else if ((this->toVec.x - this->fromVec.x) < 0.0f)
-		{
-			this->forwardXDirection = -1;
-		}
-
-		if ((this->toVec.y - this->fromVec.y) > 0.0f)
-		{
-			this->forwardYDirection = 1;
-		}
-		else if ((this->toVec.y - this->fromVec.y) < 0.0f)
-		{
-			this->forwardYDirection = -1;
-		}
-
-		if ((this->toVec.z - this->fromVec.z) > 0.0f)
-		{
-			this->forwardZDirection = 1;
-		}
-		else if ((this->toVec.z - this->fromVec.z) < 0.0f)
-		{
-			this->forwardZDirection = -1;
-		}
-
-		// set backward directions
-		if ((this->fromVec.x - this->toVec.x) > 0.0f)
-		{
-			this->backwardXDirection = 1;
-		}
-		else if ((this->fromVec.x - this->toVec.x) < 0.0f)
-		{
-			this->backwardXDirection = -1;
-		}
-
-		if ((this->fromVec.y - this->toVec.y) > 0.0f)
-		{
-			this->backwardYDirection = 1;
-		}
-		else if ((this->fromVec.y - this->toVec.y) < 0.0f)
-		{
-			this->backwardYDirection = -1;
-		}
-
-		if ((this->fromVec.z - this->toVec.z) > 0.0f)
-		{
-			this->backwardZDirection = 1;
-		}
-		else if ((this->fromVec.z - this->toVec.z) < 0.0f)
-		{
-			this->backwardZDirection = -1;
-		}
-	}
-
-	void Tweener::setSpeeds()
-	{
-		// find the component which has the greatest distance, this component will use the actual speed, while
-		// the others will use the product of the percentage of their distance and the greatest distance and the actual speed
-		float xDis = fabs(this->toVec.x - this->fromVec.x);
-		float yDis = fabs(this->toVec.y - this->fromVec.y);
-		float zDis = fabs(this->toVec.z - this->fromVec.z);
-
-		if (xDis >= yDis && xDis >= zDis)
-		{
-			this->speeds.x = this->speed;
-			this->speeds.y = yDis > 0.0f ? ((yDis / xDis) * this->speed) : 0.0f;
-			this->speeds.z = zDis > 0.0f ? ((zDis / xDis) * this->speed) : 0.0f;
-		}
-		else if (yDis >= xDis && yDis >= zDis)
-		{
-			this->speeds.x = xDis > 0.0f ? ((xDis / yDis) * this->speed) : 0.0f;
-			this->speeds.y = this->speed;
-			this->speeds.z = zDis > 0.0f ? ((zDis / yDis) * this->speed) : 0.0f;
-		}
-		else if (zDis >= xDis && zDis >= yDis)
-		{
-			this->speeds.x = xDis > 0.0f ? ((xDis / zDis) * this->speed) : 0.0f;
-			this->speeds.y = yDis > 0.0f ? ((yDis / zDis) * this->speed) : 0.0f;
-			this->speeds.z = this->speed;
-		}
-	}
-
 	void Tweener::updateSpeed(float newSpeed)
 	{
 		this->speed = newSpeed;
-		this->setSpeeds();
 	}
 
 }
